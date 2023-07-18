4be42105dbfe75a5ed616c12650900209b4c1d30
ian.dover <ian.dover@skunova.com>
2023-07-18 18:14:08 +0000

    cleaned the openwebtext data

--- openwebtextcleaning.sh
+++ openwebtextcleaning.sh
@@ -3,7 +3,7 @@
 folder="data/masking/openwebtext/openwebtext"
 
 # Navigate to the specified folder
-cd "$folder"
+cd "$folder" || exit
 
 # iterate over every file in the folder
 for file in *; do
@@ -11,5 +11,25 @@ for file in *; do
     if [[ -f "$file" ]]; then
         # remove all non-ASCII characters and overwrite the file
         tr -cd '\11\12\15\40-\176' < "$file" > temp && mv temp "$file"
+    fi
+done
+
+i=0
+# iterate over every file in the folder
+for file in *; do
+    # check if it's a file (not a directory)
+    if [[ -f "$file" ]]; then
+        # rename files and provide .txt extention
+        mv -- "$file" "batch_${i}.txt"
+        ((i++))
+    fi
+done
+
+# iterate over every file in the folder
+for file in *.txt; do
+    # check if it's a file (not a directory)
+    if [[ -f "$file" ]]; then
+        # remove extra whitespace lines from the .txt files
+        sed -i '/^$/d' "$file"
     fi
 done
