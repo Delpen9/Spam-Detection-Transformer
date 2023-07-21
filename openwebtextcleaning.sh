5c6bd9ee3745cfbe301adf61d4c5786e5d8db75b
ian.dover <ian.dover@skunova.com>
2023-07-21 19:06:06 +0000

    changed code for openwebtext cleaning

--- openwebtextcleaning.sh
+++ openwebtextcleaning.sh
@@ -6,54 +6,61 @@ folder="data/masking/openwebtext/openwebtext"
 cd "$folder" || exit
 
 # iterate over every file in the folder
-for file in *; do
-    # check if it's a file (not a directory)
-    if [[ -f "$file" ]]; then
-        # remove all non-ASCII characters and overwrite the file
-        tr -cd '\11\12\15\40-\176' < "$file" > temp && mv temp "$file"
-    fi
+for file in *.xz; do
+    # De-compress the .xz files
+    # This could potentially take a long time
+    xz -d "$file"
 done
 
-i=0
-# iterate over every file in the folder
-for file in *; do
-    # check if it's a file (not a directory)
-    if [[ -f "$file" ]]; then
-        # rename files and provide .txt extention
-        mv -- "$file" "batch_${i}.txt"
-        ((i++))
-    fi
-done
+# # iterate over every file in the folder
+# for file in *; do
+#     # check if it's a file (not a directory)
+#     if [[ -f "$file" ]]; then
+#         # remove all non-ASCII characters and overwrite the file
+#         tr -cd '\11\12\15\40-\176' < "$file" > temp && mv temp "$file"
+#     fi
+# done
 
-# iterate over every file in the folder
-for file in *.txt; do
-    # check if it's a file (not a directory)
-    if [[ -f "$file" ]]; then
-        # remove extra whitespace lines from the .txt files
-        sed -i '/^$/d' "$file"
-    fi
-done
+# i=0
+# # iterate over every file in the folder
+# for file in *; do
+#     # check if it's a file (not a directory)
+#     if [[ -f "$file" ]]; then
+#         # rename files and provide .txt extention
+#         mv -- "$file" "batch_${i}.txt"
+#         ((i++))
+#     fi
+# done
 
-# iterate over each .txt file in the directory
-for file in *.txt; do
-    # cut each line to a maximum of 512 characters and overwrite the original file
-    cut -c -512 "${file}" > "${file}.tmp" && mv "${file}.tmp" "${file}"
-done
+# # iterate over every file in the folder
+# for file in *.txt; do
+#     # check if it's a file (not a directory)
+#     if [[ -f "$file" ]]; then
+#         # remove extra whitespace lines from the .txt files
+#         sed -i '/^$/d' "$file"
+#     fi
+# done
 
-# Find all .txt files in the directory
-for file in *.txt; do
-    # Use awk to remove the last word of each line and output to a temp file
-    awk '{$NF=""; print $0}' "$file" > "${file}.tmp"
+# # iterate over each .txt file in the directory
+# for file in *.txt; do
+#     # cut each line to a maximum of 512 characters and overwrite the original file
+#     cut -c -512 "${file}" > "${file}.tmp" && mv "${file}.tmp" "${file}"
+# done
 
-    # Overwrite the original file with the temp file
-    mv "${file}.tmp" "$file"
-done
+# # Find all .txt files in the directory
+# for file in *.txt; do
+#     # Use awk to remove the last word of each line and output to a temp file
+#     awk '{$NF=""; print $0}' "$file" > "${file}.tmp"
 
-# Find all .txt files in the directory
-for file in *.txt; do
-    # Use tr to delete all punctuation from the file and output to a temp file
-    tr -d '[:punct:]' < "$file" > "${file}.tmp"
+#     # Overwrite the original file with the temp file
+#     mv "${file}.tmp" "$file"
+# done
 
-    # Overwrite the original file with the temp file
-    mv "${file}.tmp" "$file"
-done
+# # Find all .txt files in the directory
+# for file in *.txt; do
+#     # Use tr to delete all punctuation from the file and output to a temp file
+#     tr -d '[:punct:]' < "$file" > "${file}.tmp"
+
+#     # Overwrite the original file with the temp file
+#     mv "${file}.tmp" "$file"
+# done
