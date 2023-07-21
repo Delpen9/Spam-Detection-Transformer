bc8cd755d9e8e8be9d30e08e0e99b0ff7d9d4903
ian.dover <ian.dover@skunova.com>
2023-07-21 19:57:46 +0000

    improved openwebtext preprocessing

--- openwebtextcleaning.sh
+++ openwebtextcleaning.sh
@@ -6,17 +6,17 @@ folder="data/masking/openwebtext/openwebtext"
 cd "$folder" || exit
 
 # iterate over every file in the folder
-for file in *.xz; do
-    # De-compress the .xz files
-    # This could potentially take a long time
-    xz -d "$file"
-done
+# for file in *.xz; do
+#     # De-compress the .xz files
+#     # This could potentially take a long time
+#     xz -d "$file"
+# done
 
 # # iterate over every file in the folder
-# for file in *; do
-#     # remove all non-ASCII characters and overwrite the file
-#     tr -cd '\11\12\15\40-\176' < "$file" > temp && mv temp "$file"
-# done
+for file in *; do
+    # remove all non-ASCII characters and overwrite the file
+    tr -cd '\11\12\15\40-\176' < "$file" > temp && mv temp "$file"
+done
 
 # i=0
 # # iterate over every file in the folder
@@ -37,32 +37,8 @@ done
 #     # Create temp file
 #     tmp_file="${file}.tmp"
 
-#     # remove extra whitespace lines from the .txt files
+#     # remove lines from .txt with less than 100 characters
 #     grep -E '.{100,}' "$file" > "$tmp_file"
 
 #     mv "$tmp_file" "$file"
-# done
-
-# # iterate over each .txt file in the directory
-# for file in *.txt; do
-#     # cut each line to a maximum of 512 characters and overwrite the original file
-#     cut -c -512 "${file}" > "${file}.tmp" && mv "${file}.tmp" "${file}"
-# done
-
-# # Find all .txt files in the directory
-# for file in *.txt; do
-#     # Use awk to remove the last word of each line and output to a temp file
-#     awk '{$NF=""; print $0}' "$file" > "${file}.tmp"
-
-#     # Overwrite the original file with the temp file
-#     mv "${file}.tmp" "$file"
-# done
-
-# # Find all .txt files in the directory
-# for file in *.txt; do
-#     # Use tr to delete all punctuation from the file and output to a temp file
-#     tr -d '[:punct:]' < "$file" > "${file}.tmp"
-
-#     # Overwrite the original file with the temp file
-#     mv "${file}.tmp" "$file"
 # done
