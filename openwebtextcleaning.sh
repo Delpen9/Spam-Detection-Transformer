139dd17c36258d3f3fa87ee26c8e6e3da425c017
ian.dover <ian.dover@skunova.com>
2023-07-21 19:26:31 +0000

    working on bash script

--- openwebtextcleaning.sh
+++ openwebtextcleaning.sh
@@ -6,31 +6,26 @@ folder="data/masking/openwebtext/openwebtext"
 cd "$folder" || exit
 
 # iterate over every file in the folder
-for file in *.xz; do
-    # De-compress the .xz files
-    # This could potentially take a long time
-    xz -d "$file"
+# for file in *.xz; do
+#     # De-compress the .xz files
+#     # This could potentially take a long time
+#     xz -d "$file"
+# done
+
+# iterate over every file in the folder
+# for file in *; do
+#     # remove all non-ASCII characters and overwrite the file
+#     tr -cd '\11\12\15\40-\176' < "$file" > temp && mv temp "$file"
+# done
+
+i=0
+# iterate over every file in the folder
+for file in *; do
+    # rename files and provide .txt extension
+    mv -- "$file" "batch_${i}.txt"
+    
+    i++
 done
-
-# # iterate over every file in the folder
-# for file in *; do
-#     # check if it's a file (not a directory)
-#     if [[ -f "$file" ]]; then
-#         # remove all non-ASCII characters and overwrite the file
-#         tr -cd '\11\12\15\40-\176' < "$file" > temp && mv temp "$file"
-#     fi
-# done
-
-# i=0
-# # iterate over every file in the folder
-# for file in *; do
-#     # check if it's a file (not a directory)
-#     if [[ -f "$file" ]]; then
-#         # rename files and provide .txt extention
-#         mv -- "$file" "batch_${i}.txt"
-#         ((i++))
-#     fi
-# done
 
 # # iterate over every file in the folder
 # for file in *.txt; do
--- training/training.py
+++ training/training.py
@@ -6,6 +6,43 @@ import os
 import torch
 import numpy as np
 
+def get_batch(
+    batch_size : int = 32
+) -> tuple[list, list, list]:
+    '''
+    '''
+    # Initialize output values
+    sentences = []
+    masked_sentences = []
+    mask_locations =  []
+
+    # Get file index
+    file_sample_index = np.random.choice(len(masked_sentence), 1)[0]
+
+    for filename in os.listdir(directory_path)[file_sample_index]:
+        if filename.endswith('.txt'):
+            with open(os.path.join(directory_path, filename), 'r') as f:
+                # Get all sentences in file
+                file_sentences = f.read().lower().strip().split('\n')
+
+                # Get batch
+                batch_indices = np.random.choice(len(masked_sentence), batch_size, replace = False)
+                batch_sentences = [file_sentences[batch_idx] for batch_idx in batch_indices]
+
+                # Create tokens
+                sentences.extend([sentence.split() for sentence in batch_sentences])
+                masked_sentences.extend([sentence.split() for sentence in batch_sentences])
+
+    # Perform masking
+    for masked_sentence in masked_sentences:
+        index = np.random.choice(len(masked_sentence), 1)[0]
+        masked_sentence[index] = '[MASK]'
+
+        # Maintain list of mask indices
+        mask_locations.append(index)
+
+    return (sentences, masked_sentences, mask_locations)
+
 if __name__ == '__main__':
     np.random.seed(1234)
 
@@ -13,6 +50,7 @@ if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     NUM_TOKENS = 30522
+    BATCH_SIZE = 32
 
     # Load the model
     model = Transformer(vocab_size = 30522, embed_dim = 768, num_heads = 12, ff_dim = 3072, num_blocks = 12, dropout = 0.1)
@@ -20,24 +58,8 @@ if __name__ == '__main__':
     # Load the BERT tokenizer
     tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
 
-    # Initialize a list to hold the sentences
-    sentences = []
-    masked_sentences = []
-    mask_locations =  []
-
-    # Read all .txt files in the directory
-    for filename in os.listdir(directory_path)[:2]:
-        if filename.endswith('.txt'):
-            with open(os.path.join(directory_path, filename), 'r') as f:
-                file_sentences = f.read().lower().strip().split('\n')
-                sentences.extend([sentence.split() for sentence in file_sentences])
-                masked_sentences.extend([masked_sentence.split() for masked_sentence in file_sentences])
-
-    # Determine position of mask
-    for masked_sentence in masked_sentences:
-        index = np.random.choice(len(masked_sentence), 1)[0]
-        mask_locations.append(index)
-        masked_sentence[index] = '[MASK]'
+    # Get batch
+    sentences, masked_sentences, mask_locations = get_batch(BATCH_SIZE)
 
     token_ids = tokenizer.__call__(
         masked_sentences[0],
