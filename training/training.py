6d94fd6c9ee9a95b37b9d552c606f55bc1abeb4f
ian.dover <ian.dover@skunova.com>
2023-07-26 20:14:35 +0000

    added docstring

--- training/training.py
+++ training/training.py
@@ -25,7 +25,7 @@ import math
 # Serialize and De-Serialize Model
 from joblib import dump
 
-class Trainer:
+class MLMTrainer:
     def __init__(
         self,
         device, model, optimizer, criterion,
@@ -35,6 +35,27 @@ class Trainer:
         SAVE_OUTPUT = False, SAVE_MODEL = False,
         TRAINING_OUTPUT_PATH = '', MODEL_OUTPUT_PATH = ''):
         '''
+        Initialize the MLMTrainer instance with specified parameters.
+
+        Parameters:
+        device: Device to train the model on. 
+        model: Instance of the model to train.
+        optimizer: Optimizer to use for training.
+        criterion: Criterion to calculate the loss.
+        tokenizer: Tokenizer to tokenize the sentences.
+        MASK_ID: ID for the mask token.
+        MASK_RATIO: Ratio of tokens to mask in a sentence.
+        NUM_EPOCHS: Number of epochs for training.
+        NUM_ITERATIONS: Number of iterations for training.
+        BATCH_SIZE: Size of the batch for training.
+        MAX_LENGTH: Maximum length for the tokenized sentence.
+        directory_path: Path of the directory containing data.
+        VALIDATION_RATIO: Ratio of data to use for validation.
+        VALIDATION_COUNT: Count of data to use for validation.
+        SAVE_OUTPUT: Flag to decide whether to save training output or not.
+        SAVE_MODEL: Flag to decide whether to save the model or not.
+        TRAINING_OUTPUT_PATH: Path to save the training output.
+        MODEL_OUTPUT_PATH: Path to save the trained model.
         '''
         super().__init__()
         self.device = device
@@ -65,6 +86,12 @@ class Trainer:
         self.validation_output = pd.DataFrame([], columns = ['epoch', 'iteration', 'loss'])
 
     def get_training_batch(self):
+        '''
+        Generate a batch of sentences for training from the directory specified in directory_path.
+
+        Returns:
+        List of sentences for training.
+        '''
         sentences = []
         directory_list = os.listdir(self.directory_path)
 
@@ -88,6 +115,12 @@ class Trainer:
         return sentences
 
     def get_validation_samples(self):
+        '''
+        Get samples for validation from the directory specified in directory_path.
+
+        Returns:
+        List of sentences for validation.
+        '''
         sentences = []
         directory_list = os.listdir(self.directory_path)
 
@@ -103,12 +136,27 @@ class Trainer:
         return sentences
     
     def chunks(self, sentences):
+        '''
+        Generator to yield chunks of sentences of size BATCH_SIZE.
+
+        Parameters:
+        sentences: List of sentences to split into chunks.
+
+        Returns:
+        Generator yielding chunks of sentences of size BATCH_SIZE.
+        '''
         for i in range(0, len(sentences), self.BATCH_SIZE):
             chunk = sentences[i : i + self.BATCH_SIZE]
             if len(chunk) == self.BATCH_SIZE:
                 yield chunk
 
     def calculate_validation_loss(self):
+        '''
+        Calculate validation loss for the current state of the model.
+
+        Returns:
+        Validation loss.
+        '''
         self.model.eval()
 
         validation_loss = 0.0
@@ -133,6 +181,15 @@ class Trainer:
         return validation_loss
 
     def encode_sentences(self, sentences):
+        '''
+        Encode sentences using the specified tokenizer.
+
+        Parameters:
+        sentences: List of sentences to encode.
+
+        Returns:
+        Tuple of inputs and targets tensors.
+        '''
         input_list = []
         for sample_idx in range(self.BATCH_SIZE):
             input_ids = self.tokenizer(
@@ -151,6 +208,13 @@ class Trainer:
         return (inputs, targets)
 
     def mask_inputs(self, inputs, sentences):
+        '''
+        Mask inputs according to the specified MASK_RATIO.
+
+        Parameters:
+        inputs: Tensor of input sentences.
+        sentences: List of sentences.
+        '''
         for i in range(self.BATCH_SIZE):
             unpadded_sentence_len = len(sentences[i])
             num_masks = int(unpadded_sentence_len * self.MASK_RATIO)
@@ -159,6 +223,10 @@ class Trainer:
             inputs[i][mask_indices] = self.MASK_ID
 
     def train(self):
+        '''
+        Train the model using the specified optimizer and criterion. 
+        Save the model and output if specified.
+        '''
         for epoch in range(self.NUM_EPOCHS):
             for iteration in range(self.NUM_ITERATIONS):
                 sentences = self.get_training_batch()
@@ -282,7 +350,7 @@ if __name__ == '__main__':
 
     tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
 
-    trainer = Trainer(
+    trainer = MLMTrainer(
         device, model, optimizer, criterion,
         tokenizer, MASK_ID, MASK_RATIO,
         NUM_EPOCHS, NUM_ITERATIONS, BATCH_SIZE, SEQ_LENGTH,
