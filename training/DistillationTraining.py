21f613a55e8f6c1eeb5854ee0db4bd81d9342c7c
ian.dover <ian.dover@skunova.com>
2023-07-27 22:35:38 +0000

    started distillation loss training

--- training/DistillationTraining.py
+++ training/DistillationTraining.py
@@ -37,7 +37,8 @@ if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     EMBED_DIM = 768
-    BATCH_SIZE = 256
+    BATCH_SIZE = 2
+    MAX_LENGTH = 16
 
     MODEL_PATH = '../artifacts/MLM/model_2023-07-27_20-32-20.joblib'
     model = load(MODEL_PATH)
@@ -58,18 +59,47 @@ if __name__ == '__main__':
 
     tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
 
+    sentences = [
+        'We are very happy that we bought this car.',
+        'aquila dave marks just got a call from someone at aquila saying they disliked trading on enrononline anymore . - r'
+    ]
+
     classifier = pipeline('text-classification', model = tinyBERTModel, tokenizer = tokenizer)
-    results = classifier(['We are very happy that we bought this car.', 'aquila dave marks just got a call from someone at aquila saying they disliked trading on enrononline anymore . - r'])
+    results = classifier(sentences)
     
-    probabilities = []
+    tiny_BERT_probabilities = []
+    tiny_BERT_labels = []
     for result in results:
         if result['label'] == 'LABEL_0':
             prob = [result['score'], 1 - result['score']]
+            label = 0
         elif result['label'] == 'LABEL_1':
             prob = [1 - result['score'], result['score']]
+            label = 1
 
-        probabilities.append(prob)
+        tiny_BERT_probabilities.append(prob)
+        tiny_BERT_labels.append(label)
 
-    print(probabilities)
+    tiny_BERT_probabilities = torch.tensor(tiny_BERT_probabilities)
+    tiny_BERT_labels = torch.tensor(tiny_BERT_labels)
 
+    sentences = [sentence.split() for sentence in sentences]
+
+    input_list = []
+    for sample_idx in range(BATCH_SIZE):
+        input_ids = tokenizer(
+            sentences[sample_idx],
+            padding = 'max_length',
+            truncation = True,
+            max_length = MAX_LENGTH,
+            return_tensors = 'pt',
+            is_split_into_words = True
+        )['input_ids'][0]
+
+        input_list.append(input_ids)
+
+    inputs = torch.stack(input_list).to(device)
+
+    loss = distillationModel.loss(inputs, tiny_BERT_probabilities, tiny_BERT_labels)
+    print(loss.item())
 
